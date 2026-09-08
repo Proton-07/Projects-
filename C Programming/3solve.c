c0d870a336319fa5934c0659b82799b1d7324e7e
Proton-07 <unknownresilen@gmail.com>
2026-09-08 14:33:49 +0530

    Organized project folders

--- C Programming/3solve.c
+++ C Programming/3solve.c
@@ -4,6 +4,6 @@
          int main ()   
 {
 
-printf ("THIS IS JUST FOR THE TESTING OF THE GIT HUB REPO \n");
+printf ("THIS IS JUST FOR THE TESTING OF THE GIT HUB REPO , AND MODIFIED 👍️ \n");
       return 0;
 }         
--- C/3solve.c
+++
@@ -1,9 +0,0 @@
-//SAMPLE CODE FOR TESTING 
-
-#include <stdio.h>
-         int main ()   
-{
-
-printf ("THIS IS JUST FOR THE TESTING OF THE GIT HUB REPO , AND MODIFIED 👍️ \n");
-      return 0;
-}         
